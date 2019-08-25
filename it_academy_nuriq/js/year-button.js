bfe17dd328d6fc94dd473a67d798b6547056b19d
Nuriq G <nomminor@gmail.com>
2019-08-26 04:48:38 +0600

    first release

---
+++ it_academy_nuriq/js/year-button.js
@@ -0,0 +1,8 @@
+function showYears() {
+    var elem = document.getElementById('year_button');
+    if (elem.style.display === 'none') {
+        elem.style.display = 'block'
+    } else {
+        elem.style.display = 'none'
+    }
+}
