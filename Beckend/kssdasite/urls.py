afc996756a82f3e1cad71d9d35fddc034ff5fad0
Bakyt <mailybekov_b@iuca.kg>
2020-05-12 01:28:12 +0600

    before we

--- Beckend/kssdasite/urls.py
+++ Beckend/kssdasite/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import AlumniDetailView
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('', views.index, name='index_url'),
     path('projects/', views.projects, name='projects_url'),
     path('languages/', views.languages, name='languages_url'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('alumnus/<int:id>/', views.alumnus, name='alumnus_url'),
     path('about/', views.about, name='about_url'),
     #path('index/feedback/', views.feedback, name='feedback'),
-=======
+
     path('', views.index, name='Index'),
     path('feedback/', views.feedback, name='feedback'),
     path('projects/', views.projects, name='Projects'),
@@ -23,5 +23,5 @@ urlpatterns = [
     path('alumnus/', views.alumnus, name='alumnus'),
     path('about/', views.about, name='About'),
     path('alumni/<int:pk>/', AlumniDetailView.as_view(), name='alumni')
->>>>>>> b1cd0b3b89e58abb91f870436cb0ceec48a9eeca
+
 ]
